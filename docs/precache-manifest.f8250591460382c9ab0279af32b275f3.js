e0580a5452244ee4f2cbdb1592a15f4dce045f69
pearu <kentti.koppel@gmail.com>
2018-11-06 20:59:26 +0200

    publish webcam

---
+++ docs/precache-manifest.f8250591460382c9ab0279af32b275f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b5e1340885f1a4187e3",
+    "url": "/camerascan/static/js/runtime~main.2b5e1340.js"
+  },
+  {
+    "revision": "95ee441ac250772e3fe6",
+    "url": "/camerascan/static/js/main.95ee441a.chunk.js"
+  },
+  {
+    "revision": "d7fe1f4ddd38fbb85394",
+    "url": "/camerascan/static/js/1.d7fe1f4d.chunk.js"
+  },
+  {
+    "revision": "95ee441ac250772e3fe6",
+    "url": "/camerascan/static/css/main.211810f0.chunk.css"
+  },
+  {
+    "revision": "65feb6a6f8d084bad5aeb9b0a689f212",
+    "url": "/camerascan/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.24d029c4af755209d5eaeb971946764d.js"
+  "/camerascan/precache-manifest.f8250591460382c9ab0279af32b275f3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/camerascan/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
